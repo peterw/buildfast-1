ebc3be7ca9a30d756d3e6762a0395cc06c994dc2
wpeterr <mycodeisbad@gmail.com>
2023-05-18 23:33:00 -0400

    inital

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+
+import replicate
+import streamlit as st
+from dotenv import load_dotenv
+from elevenlabs import generate
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+
