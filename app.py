408c995b8e5366a9e098e1dcb24d4ff80399d3b1
wpeterr <mycodeisbad@gmail.com>
2023-05-20 19:57:19 -0400

    Revert "inital"
    This reverts commit 5e9b80b851867acb5dc2e630f7ee4f7a659a330b.

--- app.py
+++ app.py
@@ -1,47 +1,10 @@
-import openai
+import os
+
+import replicate
 import streamlit as st
-from streamlit_chat import message
 from dotenv import load_dotenv
-import os 
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-import openai
-from langchain.document_loaders import UnstructuredMarkdownLoader
-from langchain.chains.question_answering import load_qa_chain
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import UnstructuredPDFLoader
+from elevenlabs import generate
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
 
-load_dotenv()
-
-def generate_response(prompt):
-    completion = openai.ChatCompletion.create(
-    model="gpt-4",
-    messages=[
-        {"role": "user", "content": prompt}
-    ])
-    response = completion.choices[0].message.content
-    return response
-
-st.title("🤖 personal bot")
-
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-
-def get_text():
-    input_text = st.text_input("", key="input")
-    return input_text 
-
-user_input = get_text()
-
-if user_input:
-    output = generate_response(user_input)
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
-
-if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])):
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-        message(st.session_state["generated"][i], key=str(i))
